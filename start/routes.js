876a6bdad71c49fa92394719797c774a0b8b0ad1
Allan Winckler <awmoreira@gmail.com>
2019-03-14 10:59:46 -0300

    Acerto de rotas

--- start/routes.js
+++ start/routes.js
@@ -23,7 +23,7 @@ Route.group(() => {
   Route.get('roles', 'RoleController.index')
   Route.resource('teams', 'TeamController')
     .apiOnly()
-    .validator(new Map([['teams.store', 'teams.update'], ['Team']]))
+    .validator(new Map([[['teams.store', 'teams.update'], ['Team']]]))
 }).middleware('auth')
 
 Route.group(() => {
@@ -33,9 +33,11 @@ Route.group(() => {
 
   Route.resource('projects', 'ProjectController')
     .apiOnly()
-    .validator(new Map([['projects.store', 'projects.update'], ['Project']]))
+    .validator(new Map([[['projects.store', 'projects.update'], ['Project']]]))
     .middleware(
-      new Map([['projects.store', 'projects.update'], ['can:projects_create']])
+      new Map([
+        [['projects.store', 'projects.update'], ['can:projects_create']]
+      ])
     )
 
   Route.get('members', 'MemberController.index')
